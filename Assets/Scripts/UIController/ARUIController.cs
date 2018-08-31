b54be7bf50686c37ff3d85d928157c2bb5c8c565
ImL1s <aa22396584@gmail.com>
2018-08-31 15:21:38 +0800

    first commit

---
+++ Assets/Scripts/UIController/ARUIController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public partial class ARUIController : MonoBehaviour
+{
+    private Button btnFocus;
+    private Button btnIdle;
+    private Button btnMotion1;
+    private Button btnMotion2;
+    private Button btnToEBook;
+
+    //public Animator foxAnimator;
+    //public Animator gazelleAnimator;
+    //public Animator lemurAnimator;
+    //public Animator chameleonAnimator;
+    //public Animator boarAnimator;
+
+    public List<Animator> animators;
+
+    void Awake()
+    {
+        InitUIRef();
+        InitUIEvent();
+    }
+}
+
+/// <summary>
+/// init method
+/// </summary>
+partial class ARUIController
+{
+    public
+
+    void InitUIRef()
+    {
+        btnFocus = transform.Find("group/btn_focus").GetComponent<Button>();
+        btnIdle = transform.Find("group/btn_idle").GetComponent<Button>();
+        btnMotion1 = transform.Find("group/btn_motion1").GetComponent<Button>();
+        btnMotion2 = transform.Find("group/btn_motion2").GetComponent<Button>();
+        btnToEBook = transform.Find("group/btn_to_ebook").GetComponent<Button>();
+    }
+
+
+    private void InitUIEvent()
+    {
+        //btnFocus.onClick.AddListener(() => NextPage());
+        btnIdle.onClick.AddListener(() => PlayIdleAnimation());
+        btnMotion1.onClick.AddListener(() => PlayMotion1Animation());
+        btnMotion2.onClick.AddListener(() => PlayMotion2Animation());
+        btnToEBook.onClick.AddListener(() => NavigateToEBook());
+    }
+}
+
+/// <summary>
+/// UI event
+/// </summary>
+partial class ARUIController
+{
+    private void NavigateToEBook()
+    {
+        SceneManager.LoadScene(ConstantValue.Scenes.EBook);
+    }
+
+    private void PlayIdleAnimation()
+    {
+        foreach (var item in animators)
+        {
+            item.SetInteger("state", ConstantValue.AnimatorState.Idle);
+        }
+    }
+
+    private void PlayMotion1Animation()
+    {
+        foreach (var item in animators)
+        {
+            item.SetInteger("state", ConstantValue.AnimatorState.Motion1);
+        }
+    }
+
+    private void PlayMotion2Animation()
+    {
+        foreach (var item in animators)
+        {
+            item.SetInteger("state", ConstantValue.AnimatorState.Motion2);
+        }
+    }
+}
---
+++ Assets/Scripts/UIController/MainUIController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using System;
+using UnityEngine.SceneManagement;
+
+public partial class MainUIController : MonoBehaviour
+{
+    private Button _btnFox;
+    private Button _btnGazelle;
+    private Button _btnLemur;
+    private Button _btnChameleon;
+    private Button _btnBoar;
+
+    private readonly Subject<string> _onClickSource = new Subject<string>();
+
+    void Awake()
+    {
+        InitUiRef();
+        InitUiEvent();
+        InitRxEvent();
+    }
+}
+
+/// <summary>
+/// init
+/// </summary>
+partial class MainUIController
+{
+    void InitUiRef()
+    {
+        _btnFox = transform.Find("group/btn_fox").GetComponent<Button>();
+        _btnGazelle = transform.Find("group/btn_gazelle").GetComponent<Button>();
+        _btnLemur = transform.Find("group/btn_lemur").GetComponent<Button>();
+        _btnChameleon = transform.Find("group/btn_chameleon").GetComponent<Button>();
+        _btnBoar = transform.Find("group/btn_boar").GetComponent<Button>();
+    }
+
+    void InitUiEvent()
+    {
+        _btnFox.onClick.AddListener(() =>
+        {
+            GameController.Instance.CurrentSelectedAnimal = ConstantValue.EBookAnimal.Fox;
+            _onClickSource.OnNext(ConstantValue.Scenes.EBook);
+        });
+        _btnGazelle.onClick.AddListener(() =>
+        {
+            GameController.Instance.CurrentSelectedAnimal = ConstantValue.EBookAnimal.Gazelle;
+            _onClickSource.OnNext(ConstantValue.Scenes.EBook);
+        });
+        _btnLemur.onClick.AddListener(() =>
+        {
+            GameController.Instance.CurrentSelectedAnimal = ConstantValue.EBookAnimal.Lemur;
+            _onClickSource.OnNext(ConstantValue.Scenes.EBook);
+        });
+        _btnChameleon.onClick.AddListener(() =>
+        {
+            GameController.Instance.CurrentSelectedAnimal = ConstantValue.EBookAnimal.Chameleon;
+            _onClickSource.OnNext(ConstantValue.Scenes.EBook);
+        });
+        _btnBoar.onClick.AddListener(() =>
+        {
+            GameController.Instance.CurrentSelectedAnimal = ConstantValue.EBookAnimal.Boar;
+            _onClickSource.OnNext(ConstantValue.Scenes.EBook);
+        });
+    }
+
+    private void InitRxEvent()
+    {
+        _onClickSource
+            .ThrottleFirst(TimeSpan.FromMilliseconds(2000))
+            .Subscribe(SceneManager.LoadScene);
+    }
+
+}
